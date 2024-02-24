bdf7de497bd9d07ca7ba197c9f44d9250d15129a
jovid_18 <cshcho99@gmail.com>
2024-02-24 15:15:24 +0900

    1.7

--- layered-architecture-pattern/src/controllers/posts.controller.js
+++ layered-architecture-pattern/src/controllers/posts.controller.js
@@ -1,1 +1,29 @@
-export class PostsController {}
+import { PostsService } from '../services/posts.service';
+
+export class PostsController {
+  postsService = new PostsService();
+  //게시글 조회 API
+  getPosts = async (req, res, next) => {
+    try {
+      const posts = await this.postsService.findAllPosts();
+      return res.status(200).json({ data: posts });
+    } catch (error) {
+      next(error);
+    }
+  };
+  //게시글 생성 API
+  createPost = async (req, res, next) => {
+    try {
+      const { nickname, password, title, content } = req.body;
+      const createdPost = await this.postsService.createPost(
+        nickname,
+        password,
+        title,
+        content
+      );
+      return res.status(201).json({ data: createdPost });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
