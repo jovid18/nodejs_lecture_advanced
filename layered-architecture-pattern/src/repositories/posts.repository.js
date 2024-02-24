0946328d754331b6e602c972b6f5fd0a82145a39
jovid_18 <cshcho99@gmail.com>
2024-02-24 16:59:40 +0900

    1.10

--- layered-architecture-pattern/src/repositories/posts.repository.js
+++ layered-architecture-pattern/src/repositories/posts.repository.js
@@ -1,11 +1,26 @@
+// src/repositories/posts.repository.js
+
 import { prisma } from '../utils/prisma/index.js';
 
 export class PostsRepository {
   findAllPosts = async () => {
-    const posts = await prisma.posts.findMany({});
+    // ORM인 Prisma에서 Posts 모델의 findMany 메서드를 사용해 데이터를 요청합니다.
+    const posts = await prisma.posts.findMany();
+
     return posts;
   };
+
+  findPostById = async (postId) => {
+    // ORM인 Prisma에서 Posts 모델의 findUnique 메서드를 사용해 데이터를 요청합니다.
+    const post = await prisma.posts.findUnique({
+      where: { postId: +postId },
+    });
+
+    return post;
+  };
+
   createPost = async (nickname, password, title, content) => {
+    // ORM인 Prisma에서 Posts 모델의 create 메서드를 사용해 데이터를 요청합니다.
     const createdPost = await prisma.posts.create({
       data: {
         nickname,
@@ -14,6 +29,36 @@ export class PostsRepository {
         content,
       },
     });
+
     return createdPost;
+  };
+
+  updatePost = async (postId, password, title, content) => {
+    // ORM인 Prisma에서 Posts 모델의 update 메서드를 사용해 데이터를 수정합니다.
+    const updatedPost = await prisma.posts.update({
+      where: {
+        postId: +postId,
+        password: password,
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+
+    return updatedPost;
+  };
+
+  deletePost = async (postId, password) => {
+    // ORM인 Prisma에서 Posts 모델의 delete 메서드를 사용해 데이터를 삭제합니다.
+    console.log(+postId, password);
+    const deletedPost = await prisma.posts.delete({
+      where: {
+        postId: +postId,
+        password: password,
+      },
+    });
+
+    return deletedPost;
   };
 }
