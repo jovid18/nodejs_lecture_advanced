eab24d75a93d067e54d99905784332b70f0a6ae6
jovid_18 <cshcho99@gmail.com>
2024-02-25 17:38:56 +0900

    1.12

--- jest-example/validation.js
+++ jest-example/validation.js
@@ -1,4 +1,8 @@
 export const isEmail = (value) => {
   // value가 이메일 형식에 맞으면 true, 형식에 맞지 않으면 false를 return 하도록 구현해보세요
-  return false;
+  const email = value || '';
+  if (email.split('@').length !== 2) return false;
+  if (email.includes(' ')) return false;
+  if (email[0] === '-') return false;
+  return true;
 };
