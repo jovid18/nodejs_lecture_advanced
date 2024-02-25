58dc7f9835236c5b159001411f6c172a425aa392
jovid_18 <cshcho99@gmail.com>
2024-02-25 17:48:16 +0900

    1.14

--- jest-example/validation.js
+++ jest-example/validation.js
@@ -1,8 +1,16 @@
 export const isEmail = (value) => {
   // value가 이메일 형식에 맞으면 true, 형식에 맞지 않으면 false를 return 하도록 구현해보세요
   const email = value || '';
+  const [localPart, domain, ...etc] = email.split('@');
+
   if (email.split('@').length !== 2) return false;
   if (email.includes(' ')) return false;
   if (email[0] === '-') return false;
+  if (!/^[a-z0-9+_-]+$/gi.test(localPart)) {
+    return false;
+  }
+  if (!/^[a-z0-9.-]+$/gi.test(domain)) {
+    return false;
+  }
   return true;
 };
--- jest-example/validation.spec.js
+++ jest-example/validation.spec.js
@@ -17,3 +17,14 @@ test('입력한 이메일 주소 맨 앞에 하이픈(-)이 있으면 이메일 
   expect(isEmail('e-m-a-i-l@domain.com')).toEqual(true);
   expect(isEmail('-email@domain.com')).toEqual(false);
 });
+
+test('입력한 이메일 주소중, 로컬 파트(골뱅이 기준 앞부분)에는 영문 대소문자와 숫자, 특수문자는 덧셈기호(+), 하이픈(-), 언더바(_) 3개 외에 다른 값이 존재하면 이메일 형식이 아니다.', () => {
+  expect(isEmail('_good-Email+test99@domain.com')).toEqual(true);
+  expect(isEmail('my$bad-Email9999@domain.com')).toEqual(false);
+});
+
+test('입력한 이메일 주소중, 도메인(골뱅이 기준 뒷부분)에는 영문 대소문자와 숫자, 하이픈(-) 외에 다른 값이 존재하면 이메일 형식이 아니다.', () => {
+  expect(isEmail('my-email@my-Domain99.com')).toEqual(true);
+  expect(isEmail('my-email@my_Domain99.com')).toEqual(false);
+  expect(isEmail('my-email@my$Domain99.com')).toEqual(false);
+});
